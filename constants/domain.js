d5664ac46d67969bdab4ff09a68f8881c246d4d8
EliasGxA <eliasgarcia2206@gmail.com>
2023-02-13 20:42:54 -0800

    android release for testing - leo

--- constants/domain.js
+++ constants/domain.js
@@ -1,8 +1,11 @@
 // production
-export const BASE_URL = "https://ache-backend.herokuapp.com";
+// export const BASE_URL = "https://ache-backend.herokuapp.com";
 
 // pre-prod
 //export const BASE_URL = "https://dev-ache-backend.herokuapp.com";
+
+// pre-prod leo
+export const BASE_URL = "https://nest-learning.herokuapp.com";
 
 //payment-site
 export const BASE_URL_PAYMENT = "https://react-paymentsite.herokuapp.com/";
