49e28e534eb4d7f3484f950fafc77b5b7957c16e
EliasGxA <eliasgarcia2206@gmail.com>
2023-02-24 11:08:08 -0800

    preprod url updated - build android

--- constants/domain.js
+++ constants/domain.js
@@ -3,9 +3,10 @@
 
 // pre-prod
 //export const BASE_URL = "https://dev-ache-backend.herokuapp.com";
+export const BASE_URL = "https://ache-backend-preprod.herokuapp.com";
 
 // pre-prod leo
-export const BASE_URL = "https://nest-learning.herokuapp.com";
+//export const BASE_URL = "https://nest-learning.herokuapp.com";
 
 //payment-site
 export const BASE_URL_PAYMENT = "https://react-paymentsite.herokuapp.com/";
